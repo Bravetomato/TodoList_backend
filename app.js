24fee95144846f5ea986ce7ce45fac1b0c2948c7
Bravetomato <princeps2nd@gmail.com>
2023-03-09 19:30:59 +0900

    할일조회

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+
+// DB 설정
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "korad",
+  password: "kor123414",
+  database: "todo_2023_03",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 const app = express();
 // codepen의 요청만 허락하겠다.
@@ -12,8 +24,24 @@ app.use(cors(corsOptions));
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/:user_code/todos", async (req, res) => {
+  const {user_code} = req.params;
+
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    WEHER user_code = ?
+    ORDER BY id dDESC
+    `,
+    [user_code]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: rows,
+  })
 });
 
 //todos에 접속해서 id 1, 2의 데이터 가져오기.
